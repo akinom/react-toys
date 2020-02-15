fbda4e9fb7763390a9fc0572be50c27b805fe6d9
Monika Mevenkamp <monikam@princeton.edu>
2020-02-14 21:34:36 -0500

    level up after n consecutive correct answers

--- src/NumbersPractice.js
+++ src/NumbersPractice.js
@@ -5,15 +5,24 @@ export class TimesTable extends React.Component {
         super(props);
         this.state = {
             history: [],
-            problem: this.newProblem(),
+            problem: null,
             stats: new AnswerStats(),
-            input: ''
+            problemGenerator: new LeveledProblemGenerator(),
+            leveledUp : false,
+            input: '',
+            test: true
+        }
+        this.state.problem = this.newProblem()
+        if (this.state.test) {
+            this.state.levelUpThreshold = 3
+        } else {
+            this.state.levelUpThreshold = 10
         }
     }
 
     newProblem() {
         let p = new Problem()
-        p.state = p.seedState(2);
+        p.state = p.seedState(this.state.problemGenerator);
         return p
     }
 
@@ -23,71 +32,79 @@ export class TimesTable extends React.Component {
 
     handleSubmit(event) {
         event.preventDefault();
+        let leveledUp = false;
         let updatedProblem = this.state.problem;
         updatedProblem.answer = parseInt(this.state.input)
         this.state.history.push(updatedProblem)
         this.state.stats.countAnswer(updatedProblem.isCorrect())
-
+        if ((this.state.stats.nConsecCorrect % this.state. levelUpThreshold ) === 0 && this.state.stats.nConsecCorrect > 0) {
+            this.state.problemGenerator.levelUp()
+            leveledUp = true;
+        }
+        console.log(JSON.stringify(this.state.stats))
         this.setState({
             problem: this.newProblem(),
             history: this.state.history,
             stats: this.state.stats,
+            leveledUp: leveledUp,
             input: '',
         })
     }
 
     render() {
-        return <div>
+        let leveledUpMessage = this.state.leveledUp ? "Leveling Up !" : ''
+        return (
             <div className='offset container-sm'>
                 <div className={'row'}>
-                    <form onSubmit={this.handleSubmit.bind(this)}>
-                        <div className="input-group">
-                            <div className="input-group-prepend">
+                     <h2>  Practice Level: {this.state.problemGenerator.level +1} </h2>
+                </div>
+                <div className={'row theProblem'}>
+                        <form onSubmit={this.handleSubmit.bind(this)}>
+                            <div className="input-group">
+                                <div className="input-group-prepend">
                                     <span className="input-group-text">
                                          <ReactProblem problem={this.state.problem} showResult={false}/> &nbsp; =
                                     </span>
+                                </div>
+                                <input type="number" className="form-control" value={this.state.input}
+                                       onChange={this.trackInput.bind(this)}/>
+                                <input type="submit" style={{display: 'none'}}/>
                             </div>
-                            <input type="number" className="form-control" value={this.state.input}
-                                   onChange={this.trackInput.bind(this)}/>
-                            <input type="submit" style={{display: 'none'}}/>
-                        </div>
-                    </form>
+                        </form>
                 </div>
-            </div>
 
-            <div className='container-sm'>
-                <div className={'row'} id={'stats'}>
-                    <div className={'col alert alert-success'} id={'correct'}>
+                <div className={'row'}>
+                    <div className={'col alert alert-success shortCol'}>
                         <span> {this.state.stats.ncorrect} correct  </span>
                     </div>
-                    <div className={'col alert'} id={'progress'}>
-                        <Feedback stats={this.state.stats} test={true} />
+                    <div className={'col alert'}>
+                        <Feedback stats={this.state.stats} limit={this.state.levelUpThreshold} message={leveledUpMessage}/>
                     </div>
                 </div>
                 <div className={'row'}>
-                    <ProblemList  problems={this.state.history} />
+                    <ProblemList problems={this.state.history}/>
                 </div>
-            </div>
-        </div>;
+            </div>)
     }
 }
 
 
 class Feedback extends React.Component {
     render() {
-        let modulo = this.props.test ? 3 : 10
+        let modulo = this.props.limit
         let stats = this.props.stats
-        let feedback = ''
-        if (stats.nConsecCorrect % modulo === 0 && stats.nConsecCorrect > 0) {
-            feedback = (<span className={'alert alert-primary'}> {stats.nConsecCorrect} Consecutive Correct  </span>)
-        } else {
+        let feedback = this.props.message
+        if (feedback === '') {
             let percent = (stats.nConsecCorrect % modulo) * (100 / modulo)
+            console.log([percent, modulo]);
             feedback = (
                 <div className="progress">
                     <div className="progress-bar" role="progressbar" style={{width: percent + '%'}}
                          aria-valuenow={percent}
                          aria-valuemin="0" aria-valuemax="100"></div>
                 </div>)
+        } else {
+            feedback = (<span className={'alert alert-primary'}> {feedback}   </span>)
         }
         return feedback
     }
@@ -97,82 +114,99 @@ class ProblemList extends React.Component {
     render() {
         let problems = this.props.problems
 
-        const KLS_SELECTION = {undefined: '', true: 'alert list-group-item list-group-item-success', false: 'alert list-group-item list-group-item-danger'}
+        const KLS_SELECTION = {
+            undefined: '',
+            true: 'alert list-group-item list-group-item-success',
+            false: 'alert list-group-item list-group-item-danger'
+        }
         //let showResult = this.state.mode ==='showResults'
         let display = []
-        for (let i =problems.length -1; i >= 0; i--) {
-            let problem =  problems[i]
+        for (let i = problems.length - 1; i >= 0; i--) {
+            let problem = problems[i]
             let kls = KLS_SELECTION[problem.isCorrect()]
-            display.push(  <li key={i} className={kls}> <ReactProblem problem={problem} showResult={true} /> </li> )
+            display.push(<li key={i} className={kls}><ReactProblem problem={problem} showResult={true}/></li>)
         }
 
         return (
             <ul className="list-group problemList">
-                { display }
+                {display}
             </ul>
         )
     }
 }
 
 
-const problenLevels = [
-    [simpleTimes],
-    [simpleTimes, simpleDivision],
-    [mediumTimes, mediumDivision]
-]
+class LeveledProblemGenerator {
+    constructor() {
+        this.simpleTimes = (ths) => {
+            let left = Math.floor(Math.random() * 9) + 2
+            let right = Math.floor(Math.random() * 10) + 2
+            ths['left'] = left;
+            ths['right'] = right;
+            ths['op'] = '*';
+            ths['result'] = left * right;
+        };
 
-function simpleTimes(ths) {
-    let left = Math.floor(Math.random() * 9) + 2
-    let right = Math.floor(Math.random() * 10) + 2
-    ths['left'] = left;
-    ths['right'] = right;
-    ths['op'] = '*';
-    ths['result'] = left * right;
-}
+        this.simpleDivision = (ths) => {
+            let left = Math.floor(Math.random() * 9) + 2
+            let right = Math.floor(Math.random() * 10) + 2
+            ths['left'] = left * right
+            ths['right'] = right;
+            ths['op'] = '/';
+            ths['result'] = left;
+        }
 
-function simpleDivision(ths) {
-    let left = Math.floor(Math.random() * 9) + 2
-    let right = Math.floor(Math.random() * 10) + 2
-    ths['left'] = left * right
-    ths['right'] = right;
-    ths['op'] = '/';
-    ths['result'] = left;
-}
+        this.mediumLeftRight = (ths) => {
+            let left = Math.floor(Math.random() * 9) + 2
+            let right = Math.floor(Math.random() * 10) + 2
+            if (Math.random() < .5) {
+                left = 10 * left
+            }
+            if (Math.random() < .25) {
+                right = 10 * right
+            }
+            ths['left'] = left;
+            ths['right'] = right;
+            return ths
+        }
 
-function mediumLeftRight(ths) {
-    let left = Math.floor(Math.random() * 9) + 2
-    let right = Math.floor(Math.random() * 10) + 2
-    if (Math.random() <.5) {
-        left = 10 * left
+        this.mediumTimes = (ths) => {
+            this.mediumLeftRight(ths)
+            ths['op'] = '*';
+            ths['result'] = ths['left'] * ths['right'];
+        }
+
+        this.mediumDivision = (ths) => {
+            let lr = this.mediumLeftRight({})
+            ths['op'] = '/';
+            ths['left'] = lr.left * lr.right
+            ths['right'] = lr.right
+            ths['result'] = lr.left
+            ths['left'] = ths['result'] * ths['right']
+        }
+
+        this.level = 0
+        this.problenLevels = [
+            [this.simpleTimes],
+            [this.simpleTimes, this.simpleDivision],
+            [this.mediumTimes, this.mediumDivision]
+        ];
     }
-    if (Math.random() <.25) {
-        right = 10 * right
+
+    levelUp() {
+        this.level += 1
     }
-    ths['left'] = left;
-    ths['right'] = right;
-    return ths
-}
 
-function mediumTimes(ths) {
-    mediumLeftRight(ths)
-    ths['op'] = '*';
-    ths['result'] = ths['left']  * ths['right'];
-}
-
-function mediumDivision(ths) {
-    let lr = mediumLeftRight({})
-    ths['op'] = '/';
-    ths['left'] = lr.left * lr.right
-    ths['right'] = lr.right
-    ths['result'] = lr.left
-    ths['left'] = ths['result'] * ths['right']
+    genProblem(problem) {
+        let nchoices = this.problenLevels[this.level].length
+        let choice = Math.floor(Math.random() * nchoices)
+        this.problenLevels[this.level][choice](problem)
+    }
 }
 
 class Problem {
-    seedState(level) {
-        let nchoices = problenLevels[level].length
-        let choice = Math.floor ( Math.random() * nchoices)
-        problenLevels[level][choice](this)
+    seedState(generator) {
+        generator.genProblem(this)
         this.showResult = false
     }
 
@@ -183,15 +217,19 @@ class Problem {
             return this.answer === this.result
         }
     }
-
 }
 
+class Message {
+    render() {
+        return (<span>{this.props.message}   </span>)
+    }
+}
 
 class ReactProblem extends React.Component {
     render() {
         let problem = this.props.problem;
         let withResult = ''
-        if ( this.props.showResult)  {
+        if (this.props.showResult) {
             withResult = (<span> = {problem.result}</span>)
         }
         return (
@@ -203,9 +241,9 @@ class ReactProblem extends React.Component {
 }
 
 
-class AnswerStats  {
+class AnswerStats {
     constructor() {
-        this.ncorrect  = 0
+        this.ncorrect = 0
         this.nwrong = 0
         this.nConsecCorrect = 0
         this.lastCorrect = false
@@ -220,7 +258,6 @@ class AnswerStats  {
             this.nConsecCorrect = 0
         }
         this.lastCorrect = correct
-        window.console.log(JSON.stringify(this))
     }
 
 }
--- src/PracticeApp.js
+++ src/PracticeApp.js
@@ -4,24 +4,7 @@ import {TimesTable} from './NumbersPractice'
 
 export class App extends React.Component {
     render() {
-        return (
-            <div className={'container'}>
-                <div id={'title'} className={'row'}>
-                    <h1>
-                        Arithmetic Practice
-                    </h1>
-                </div>
-                <div className={'row'}>
-                    <div className={'container'}>
-                        <div className={'row'}>
-                            <div className='col-12 toyComponent'>
-                                <TimesTable />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
+        return (    <TimesTable /> )
     }
 }
 
